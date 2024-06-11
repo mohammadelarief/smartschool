3f1d4e6dfab68d0ce135cd9e274b499c8e15329a
mohammadelarief <mohammadelarief23@gmail.com>
2024-06-11 15:15:46 +0700

    update view

--- application/views/student/Student_list.php
+++ application/views/student/Student_list.php
@@ -2,7 +2,7 @@
   <div class="col-xs-12">
     <div class="box">
       <div class="box-header">
-        <h3 class="box-title">List Student</h3>
+        <h3 class="box-title">Daftar Siswa</h3>
         <div class="box-tools pull-right">
           <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
             <i class="fa fa-minus"></i></button>
